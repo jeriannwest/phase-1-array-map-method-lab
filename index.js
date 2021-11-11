f5cdd312d2d9af877e54013d3425626fd070ee16
+ <jeriannwest@yahoo.com>
2021-11-10 20:11:18 -0500

    commit tree f64d3e5029e53ec027127d5f1dabca5a29fb6c75

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function everyWordCap2(str) {
+  let array1 = str.split(" ");
+  let newarray1 = [];
+
+  for (let x = 0; x < array1.length; x++) {
+    newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
+  }
+  return newarray1.join(" ");
+}
+
+function titleCased() {
+  return tutorials.map((sentence) => everyWordCap2(sentence));
 }
